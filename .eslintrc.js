cbb428e878add39b0ef557dc155f4ded83d0b491
Carl-W <carl.igelstroem@tiket.com>
2020-07-23 13:17:43 +0800

    added React, babel, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  extends: ['@react-native-community', 'eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react', 'react-native', 'react-hooks'],
+  rules: { semi: [2, 'never'] },
+}
