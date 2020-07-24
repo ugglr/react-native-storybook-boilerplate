8a2915468b4c1e257a5d778ea1031f25d1ad3e13
Carl-W <carl.igelstroem@tiket.com>
2020-07-24 12:40:19 +0800

    reactjs setup complete.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'react', 'babel-plugin-styled-components'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['babel-plugin-styled-components'],
 }
---
+++ index.web.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App.web'
+
+ReactDOM.render(<App />, document.getElementById('app'))
